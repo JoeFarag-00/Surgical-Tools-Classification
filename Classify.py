635c5ac54ebf9921fff99cc38b3fb79a35253f32
Youssef Raghed Farag <88057098+JoeFarag-00@users.noreply.github.com>
2023-04-09 16:30:07 +0200

    Added confidence scores

--- Classify.py
+++ Classify.py
@@ -4,28 +4,26 @@ from PIL import Image, ImageTk
 import numpy as np
 from keras.models import load_model
 
-# Load the trained model
 model = load_model('surgical_tools_model.h5')
-
 
 def open_image():
     file_path = filedialog.askopenfilename()
     img = Image.open(file_path)
-    img = img.resize((300, 300))  # Resize the image
+    img = img.resize((300, 300)) 
     img = ImageTk.PhotoImage(img)
     label.config(image=img)
-    label.image = img  # Keep a reference to the image to prevent garbage collection
+    label.image = img 
     predict_label(file_path)
 
 def predict_label(image_path):
     img = Image.open(image_path)
-    img = img.resize((150, 150))  # Resize the image to match the input size of the model
-    img_array = np.array(img)  # Convert image to numpy array
-    img_array = img_array / 255.0  # Normalize pixel values to [0, 1]
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-    prediction = model.predict(img_array)  # Make prediction using the model
-    instrument = np.argmax(prediction)  # Get the instrument label with highest probability
-    confidence = np.max(prediction)  # Get the confidence rate of the predicted label
+    img = img.resize((150, 150)) 
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array) 
+    instrument = np.argmax(prediction)
+    confidence = np.max(prediction) 
     if instrument == 0:
         instrument_label.config(text="Prediction: Injection Syringe\nConfidence: {:.2%}".format(confidence))
     elif instrument == 1:
