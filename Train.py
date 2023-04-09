659846d23404c25e830faa67379156b5ef65fb8d
Youssef Raghed Farag <88057098+JoeFarag-00@users.noreply.github.com>
2023-04-09 06:02:38 +0200

    Added training function

---
+++ Train.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import ImageTk, Image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.optimizers import Adam
+
+NUM_CLASSES = 3
+IMG_SIZE = 150
+BATCH_SIZE = 32
+EPOCHS = 10
+
+# Define the CNN model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Create an instance of ImageDataGenerator for data preprocessing
+train_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(
+    'Dataset/train',
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='categorical')
+
+valid_datagen = ImageDataGenerator(rescale=1./255)
+valid_generator = valid_datagen.flow_from_directory(
+    'Dataset/valid',
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='categorical')
+
+# Train the model
+model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // BATCH_SIZE,
+    validation_data=valid_generator,
+    validation_steps=valid_generator.samples // BATCH_SIZE,
+    epochs=EPOCHS)
+
+# Save the trained model
+model.save('surgical_tools_model.h5')
+
+# Display a message box after training is completed
+messagebox.showinfo('Training Completed', 'Model has been trained successfully!')
